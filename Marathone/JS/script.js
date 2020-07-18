cb8fefbaaa31ac044ff2128eb338e841375a4875
Wardek <fragment374@gmail.com>
2020-07-18 20:54:38 +0300

    Добавил бургер меню + мелкие исправления ситаемости кода

--- Marathone/JS/script.js
+++ Marathone/JS/script.js
@@ -1,11 +1,10 @@
 $(document).on('submit', '#example_form', function(e) {
   e.preventDefault(); // запретим стандартное поведения отправки данных
   var data = $(this).serializeArray(); // получаем данные с формы
-
   $.ajax({
     url: 'http://marathone-of-change.com/example_script', // адрес скрипта который примeт данные
     type: 'POST', // метод передачи
-    dataType: 'JSON', // !если требуется! получить ответ как массив данных используем формат JSON
+    dataType: 'JSON', // получить ответ как массив данных используем формат JSON
     data: data // передаем массив данных
   }).done(function(res) {
     alert('Данные успешно переданы!');
